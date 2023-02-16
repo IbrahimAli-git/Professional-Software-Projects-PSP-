40671d28e7375b65b5453f08c693277ebe8566ee
ExBehemoth <117927931+ExBehemoth@users.noreply.github.com>
2023-02-16 13:07:55 +0000

    reverted to previous working version

--- server/index.js
+++ server/index.js
@@ -16,48 +16,8 @@ const io = new Server(server, {
     }
 })
 
-var pane = $('#box'), //the game box
-  box = $('#characterid'), //the character
-  wh = pane.width() - box.width(), //calculates the max distance character can go horizontally
-  wv = pane.height() - box.height(), //calculates the max distance character can go vertically
-  d = {}, //Stores key presses, the key for the current direction is set to 'true'
-  x = 10, //Movement speed
-  currentv = 200,
-  currenth = 300,
-  lastinput = 0;
-
-  function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > wh ? wh : n;
-  }
-  
-  function newv(v, a, b) { //calculates new horizontal postion, ensures it's within game bounds
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > wv ? wv : n;
-  }
-
-  setInterval(function () {
-      var vert;
-      var hor;
-      box.css({
-        left: function (i, n) {
-          var h = newh(n, 37, 39);
-          hor = h;
-          return h
-        },
-        top: function (i, n) {
-          var v = newv(n, 38, 40);
-          vert = v;
-          return v
-        }
-      });
-      
-      if (currentv !== vert || currenth !== hor) {
-        socket.emit("recieve_move", { v: vert, h: hor })
-        currentv = vert;
-        currenth = hor;
-      } 
-    }, 20);
+let  currentv = 200;
+let  currenth = 300;
 
 io.on("connection", (socket) => {
     let id = socket.id;
@@ -92,10 +52,7 @@ io.on("connection", (socket) => {
     });
 
     socket.on("send_input", (data) => {
-        d[lastinput] = false;
-        d[data] = true;
-        lastinput = data;
-        //socket.broadcast.emit("receive_input", data)
+        socket.broadcast.emit("receive_input", data)
     });
 
     socket.on("disconnect", (socket) => {
@@ -106,17 +63,13 @@ io.on("connection", (socket) => {
                 for (var i in players){
                     if (i !== 0){
                         io.to (players[i]).emit('new_host')
-
                     }
                 }
                 break;
             }
         }
-
-        
         console.log(players)
     })
-
 })
 
 server.listen(8080, "0.0.0.0", () => {
