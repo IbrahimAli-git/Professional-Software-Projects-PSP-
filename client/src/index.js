6d18265ce799a5a784691fb3e6eaa9c6d395ce49
IshaqChowdhury <ishaq8471@gmail.com>
2023-02-16 13:08:44 +0000

    testing room on index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Room from './Room';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Room/>
     <App />
   </React.StrictMode>
 );
