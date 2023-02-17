7609125296b8b3b73cd767d91dde7dd3d2dddf40
IshaqChowdhury <ishaq8471@gmail.com>
2023-02-17 13:37:16 +0000

    testing remote features

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const app = express()
 const http = require("http")
 const cors = require("cors")
 const { Server } = require("socket.io")
-app.use(cors({ origin: "*" }))
+app.use(cors())
 const players = [0, 0, 0, 0]
 
 const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://2a7e-143-52-64-137.eu.ngrok.io",
         methods: ["GET", "POST"]
     }
 })
