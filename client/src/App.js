818171cbc2a22ccb71e8d4cf019e990be6609b35
IbrahimAli-git <ibrahim2ali169@gmail.com>
2023-02-17 14:09:29 +0000

    ready for vercel

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import $ from 'jquery'
 import io from "socket.io-client"
-const socket = io.connect("http://10.72.196.155:8080")
+const socket = io.connect("http://localhost:8080")
 
 function App() {
 }
