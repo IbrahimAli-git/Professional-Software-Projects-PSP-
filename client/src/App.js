ca09505dedbb0bc0bd37a119edfe80166a1f66da
ExBehemoth <c0011779@hallam.shu.ac.uk>
2023-03-27 11:04:52 +0100

    made wall collision and reset to start

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,137 @@
+// import './App.css';
+// import $ from 'jquery'
+// import io from "socket.io-client"
+// const socket = io.connect("http://13.40.174.86:3000")
+// // connects clients with server using current local ip address on port 8080
+// // port 8080 used instead of 3000
+
+// function App() {
+// }
+
+// var pane = $('#box'), //the game box
+//     box = $('#characterid'), //the character
+// //   wh = pane.width() - box.width(), //calculates the max distance character can go horizontally
+// //   wv = pane.height() - box.height(), //calculates the max distance character can go vertically
+// //   d = {}, //Stores key presses, the key for the current direction is set to 'true'
+// //   x = 3, //Movement speed
+// //   currentv = 200,
+// //   currenth = 300,
+//      playernum = 0;
+// //   lastinput = 0,
+// //   host = false; //if host = true, then that client is the one doing the movement
+
+// socket.on("receive_index", (num) => { //every client that connects recieves a player number from 0-4 (0 if there are already 4 players)
+//   playernum = num;
+//   console.log("playernum: " + playernum)
+// });
+
+// // function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
+// //   var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+// //   return n < 0 ? 0 : n > wh ? wh : n;
+// // }
+
+// // function newv(v, a, b) { //calculates new horizontal postion, ensures it's within game bounds
+// //   var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+// //   return n < 0 ? 0 : n > wv ? wv : n;
+// // }
+
+// // setInterval(function () { // updates and sends new position to server at a set interval
+// //   if (host === true) {
+// //     var vert;
+// //     var hor;
+// //     box.css({
+// //       left: function (i, n) {
+// //         var h = newh(n, 37, 39);
+// //         hor = h;
+// //         return h
+// //       },
+// //       top: function (i, n) {
+// //         var v = newv(n, 38, 40);
+// //         vert = v;
+// //         return v
+// //       }
+// //     });
+
+// //     if (currentv !== vert || currenth !== hor) {
+// //       socket.emit("send_move", { v: vert, h: hor })
+// //       currentv = vert;
+// //       currenth = hor;
+// //     }
+// //   }
+// // }, 20); // interval 20ms
+  
+// socket.on("receive_move", (data) => { //recieves new position from the server
+//   var v = data.v;
+//   var h = data.h;
+
+//   console.log("received")
+//   // d[data] = true;
+//   console.log(data.v, data.h)
+//   box.css({
+//     left: h,
+//     top: v
+//   });
+//   // currentv = v;
+//   // currenth = h;
+//   // d[data] = false;
+// });
+
+// $(window).keydown(function (e) { //when a key is pressed, it checks whether that player is allowed to use that key, then sends it to the server
+//   console.log("event: " + e.which)
+//   console.log("playernum: " + playernum)
+
+//   if ((e.which === 37 || e.which === 65) && playernum === 1) {
+//     // if (37 !== lastinput) {
+//     //   d[lastinput] = false;
+//     //   d[37] = true;
+//     //   lastinput = 37;
+//     socket.emit("send_input", (37))
+//     // }
+//   }
+//   if ((e.which === 38 || e.which === 87) && playernum === 2) {
+//     // if (38 !== lastinput) {
+//     //   d[lastinput] = false;
+//     //   d[38] = true;
+//     //   lastinput = 38;
+//     socket.emit("send_input", (38))
+//     // }
+//   }
+//   if ((e.which === 39 || e.which === 68) && playernum === 3) {
+//     // if (39 !== lastinput) {
+//     //   d[lastinput] = false;
+//     //   d[39] = true;
+//     //   lastinput = 39;
+//     socket.emit("send_input", (39))
+//     // }
+//   }
+//   if ((e.which === 40 || e.which === 83) && playernum === 4) {
+//     // if (40 !== lastinput) {
+//     //   d[lastinput] = false;
+//     //   d[40] = true;
+//     //   lastinput = 40;
+//     socket.emit("send_input", (40))
+//     // }
+//   }
+//   // console.log(lastinput);
+// });
+
+// // socket.on("receive_input", (data) => { //recieves key press from server
+// //   d[lastinput] = false;
+// //   d[data] = true;
+// //   lastinput = data;
+// // });
+
+// // socket.on("new_host", () => { //server has designated a new 'host'
+// //   host = true;
+// //   console.log("host" + host);
+// // });
+
+// export default App;
+
 import './App.css';
 import $ from 'jquery'
 import io from "socket.io-client"
-const socket = io.connect("http://13.40.174.86:3000")
+const socket = io.connect("http://localhost:8080")
 // connects clients with server using current local ip address on port 8080
 // port 8080 used instead of 3000
 
@@ -10,55 +140,106 @@ function App() {
 
 var pane = $('#box'), //the game box
     box = $('#characterid'), //the character
-//   wh = pane.width() - box.width(), //calculates the max distance character can go horizontally
-//   wv = pane.height() - box.height(), //calculates the max distance character can go vertically
-//   d = {}, //Stores key presses, the key for the current direction is set to 'true'
-//   x = 3, //Movement speed
-//   currentv = 200,
-//   currenth = 300,
-     playernum = 0;
-//   lastinput = 0,
-//   host = false; //if host = true, then that client is the one doing the movement
+    wh = pane.width() - box.width(), //calculates the max distance character can go horizontally
+    wv = pane.height() - box.height(), //calculates the max distance character can go vertically
+    d = {}, //Stores key presses, the key for the current direction is set to 'true'
+    x = 3, //Movement speed
+    currentv = 10,
+    currenth = 10,
+    playernum = 0,
+    lastinput = 0,
+    host = true, //if host = true, then that client is the one doing the movement
 
-socket.on("receive_index", (num) => { //every client that connects recieves a player number from 0-4 (0 if there are already 4 players)
-  playernum = num;
-  console.log("playernum: " + playernum)
-});
+    walls = [/*[250,300,400,500],[100,300,0,190]*/];
 
-// function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
-//   var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-//   return n < 0 ? 0 : n > wh ? wh : n;
-// }
+// socket.on("receive_index", (num) => { //every client that connects recieves a player number from 0-4 (0 if there are already 4 players)
+//   playernum = num;
+//   console.log("playernum: " + playernum)
+// });
 
-// function newv(v, a, b) { //calculates new horizontal postion, ensures it's within game bounds
-//   var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-//   return n < 0 ? 0 : n > wv ? wv : n;
-// }
+function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
+  var newh = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+  if (newh < 0){
+    reset();
+    return 10;
+  }
+  else if (newh > wh){
+    reset();
+    return 10;
+  }
+  
+  if (!wallCheckH(newh)){
+    return newh;
+  }
+  else{
+    return currenth;
+  }
+}
 
-// setInterval(function () { // updates and sends new position to server at a set interval
-//   if (host === true) {
-//     var vert;
-//     var hor;
-//     box.css({
-//       left: function (i, n) {
-//         var h = newh(n, 37, 39);
-//         hor = h;
-//         return h
-//       },
-//       top: function (i, n) {
-//         var v = newv(n, 38, 40);
-//         vert = v;
-//         return v
-//       }
-//     });
+function newv(v, a, b) { //calculates new horizontal postion, ensures it's within game bounds
+  var newv = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+  if (newv < 0){
+    reset();
+    return 10;
+  }
+  else if (newv > wv){
+    reset();
+    return 10;
+  }
+  
+  if (!wallCheckV(newv)){
+    return newv;
+  }
+  else{
+    return currentv;
+  }
+}
 
-//     if (currentv !== vert || currenth !== hor) {
-//       socket.emit("send_move", { v: vert, h: hor })
-//       currentv = vert;
-//       currenth = hor;
-//     }
-//   }
-// }, 20); // interval 20ms
+function wallCheckH(x){
+  return walls.some(i => i[0] < currentv && currentv < i[1] && i[2] < x && x < i[3])
+}
+
+function wallCheckV(x){
+  return walls.some(i => i[0] < x && x < i[1] && i[2] < currenth && currenth < i[3])
+}
+
+function reset(){
+  currentv = 10;
+  currenth = 10;
+  box.css({
+    left: function (i,n) {
+      return 10
+    },
+    top: function (i,n) {
+      return 10
+    }
+  })
+}
+
+setInterval(function () { // updates and sends new position to server at a set interval
+  if (host === true) {
+    var vert;
+    var hor;
+    box.css({
+      left: function (i, n) {
+        var h = newh(n, 37, 39);
+        hor = h;
+        return h
+      },
+      top: function (i, n) {
+        var v = newv(n, 38, 40);
+        vert = v;
+        return v
+      }
+    });
+
+    if (currentv !== vert || currenth !== hor) {
+      // socket.emit("send_move", { v: vert, h: hor })
+      currentv = vert;
+      currenth = hor;
+    }
+  }
+}, 20); // interval 20ms
   
 socket.on("receive_move", (data) => { //recieves new position from the server
   var v = data.v;
@@ -80,37 +261,37 @@ $(window).keydown(function (e) { //when a key is pressed, it checks whether that
   console.log("event: " + e.which)
   console.log("playernum: " + playernum)
 
-  if ((e.which === 37 || e.which === 65) && playernum === 1) {
-    // if (37 !== lastinput) {
-    //   d[lastinput] = false;
-    //   d[37] = true;
-    //   lastinput = 37;
-    socket.emit("send_input", (37))
-    // }
+  if ((e.which === 37 || e.which === 65) /*&& playernum === 1*/) {
+    if (37 !== lastinput) {
+      d[lastinput] = false;
+      d[37] = true;
+      lastinput = 37;
+    // socket.emit("send_input", (37))
+    }
   }
-  if ((e.which === 38 || e.which === 87) && playernum === 2) {
-    // if (38 !== lastinput) {
-    //   d[lastinput] = false;
-    //   d[38] = true;
-    //   lastinput = 38;
-    socket.emit("send_input", (38))
-    // }
+  if ((e.which === 38 || e.which === 87) /*&& playernum === 2*/) {
+    if (38 !== lastinput) {
+      d[lastinput] = false;
+      d[38] = true;
+      lastinput = 38;
+    // socket.emit("send_input", (38))
+    }
   }
-  if ((e.which === 39 || e.which === 68) && playernum === 3) {
-    // if (39 !== lastinput) {
-    //   d[lastinput] = false;
-    //   d[39] = true;
-    //   lastinput = 39;
-    socket.emit("send_input", (39))
-    // }
+  if ((e.which === 39 || e.which === 68) /*&& playernum === 3*/) {
+    if (39 !== lastinput) {
+      d[lastinput] = false;
+      d[39] = true;
+      lastinput = 39;
+    // socket.emit("send_input", (39))
+    }
   }
-  if ((e.which === 40 || e.which === 83) && playernum === 4) {
-    // if (40 !== lastinput) {
-    //   d[lastinput] = false;
-    //   d[40] = true;
-    //   lastinput = 40;
-    socket.emit("send_input", (40))
-    // }
+  if ((e.which === 40 || e.which === 83) /*&& playernum === 4*/) {
+    if (40 !== lastinput) {
+      d[lastinput] = false;
+      d[40] = true;
+      lastinput = 40;
+    //socket.emit("send_input", (40))
+    }
   }
   // console.log(lastinput);
 });
