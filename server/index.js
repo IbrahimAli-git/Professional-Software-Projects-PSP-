180647bf5cec843b874e866bf9422412df7683dd
IbrahimAli-git <ibrahim2ali169@gmail.com>
2023-03-29 10:15:10 +0100

    Merged objectives with main

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express()
 const http = require("http")
 const cors = require("cors")
 const {Server } = require("socket.io") // express server created using cors
+const { cursorTo } = require("readline")
 app.use(cors({ origin: "*" })) // allows for all connections 
 
 const players = [0, 0, 0, 0] // stores a fixed no of players which are assigned a number
@@ -24,27 +25,128 @@ let paneW = 800,
     wv = paneV - boxV, //calculates the max distance character can go vertically
     d = {}, //Stores key presses, the key for the current direction is set to 'true'
     x = 3, //Movement speed
-    currentv = 200,
-    currenth = 300,
-    lastinput = 0;
+    startv = 65,
+    starth = 35,
+    currentv = startv,
+    currenth = starth,
+    lastinput = 0,
+    moveV = startv,
+    moveH = starth,
+    score = 0,
+    items = [],
+    hasReset = false,
+    itemStates = [],
+    walls = [[8,50,2,798],[171,224,228,516],[78,368,2,53],[257,313,62,348],[86,141,396,686],[109,289,726,792]];//wall boundaries currently hard coded
 
-function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > wh ? wh : n;
+    function newh(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
+        var newh = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+        if (newh < 0){
+            reset();
+            return 35;
+        }
+        else if (newh > wh){
+            reset();
+            return 35;
+        }
+        
+        if (!wallCheckH(newh)){
+            return newh;
+        }
+        else{
+            if (newh > currenth){
+                d[lastinput] = false;
+                d[37] = true;
+                lastinput = 37;
+                return currenth;
+            }
+            else{
+                d[lastinput] = false;
+                d[39] = true;
+                lastinput = 39;
+                return currenth
+            };
+        }
+    }
+        
+    function newv(v, a, b) { //calculates new vertical postion, ensures it's within game bounds
+        var newv = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+        if (newv < 0){
+            reset();
+            return 65;
+        }
+        else if (newv > wv){
+            reset();
+            return 65;
+        }
+        
+        if (!wallCheckV(newv)){
+            return newv;
+        }
+        else{
+            if (newv > currentv){
+                d[lastinput] = false;
+                d[38] = true;
+                lastinput = 38;
+                return currentv;
+            }
+            else{
+                d[lastinput] = false;
+                d[40] = true;
+                lastinput = 40;
+                return currentv;
+            }
+        }
+    }
+
+function wallCheckH(x){
+    return walls.some(i => i[0] < currentv && currentv < i[1] && i[2] < x && x < i[3])
 }
 
-function newv(v, a, b) { //calculates new horizontal postion, ensures it's within game bounds
-    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-    return n < 0 ? 0 : n > wv ? wv : n;
+function wallCheckV(x){
+    return walls.some(i => i[0] < x && x < i[1] && i[2] < currenth && currenth < i[3])
+}
+
+function itemCheck(v,h) { // searches through items array for specific item
+    for(const item of items){
+        if((item[0] <= h && h <= item[1] && item[2] <= v && v <= item[3] && item[4] == true)){
+            item[4] = false;
+            // console.log(item.indexOf(item) + 2);
+            return items.indexOf(item) + 1;
+        }
+    }
+    return 0;
+}
+
+function reset(){
+    currentv = startv;
+    currenth = starth;
+    hasReset = true;
 }
 
 io.on("connection", (socket) => { // creates socket.io connection
     let id = socket.id; // id of current client
     let playernum = 0; // assigns a no to each client
-    
+
     socket.emit("receive_move", { v: currentv, h: currenth }) // sends current vertical and horizontal to client
     console.log("User connected: " + id)
 
+    socket.on("send_items", (data) => {
+        if(items.length == 0){
+            items.push(data.d1);
+            items.push(data.d2);
+            items.push(data.d3);
+            items.push(data.d4);
+            items.push(data.d5);
+        }
+        itemStates = []
+        for(const item of items){
+            itemStates.push(item[4]);
+        }
+        console.log(items);
+    })
+    
+    
+    
     
     if (players[0] !== 0 && players[1] !== 0 && players[2] !== 0 && players[3] !== 0) { // checks if room is full
         console.log("Room is full")
@@ -53,9 +155,6 @@ io.on("connection", (socket) => { // creates socket.io connection
         for (var i = 0; i < 4; i++) {
             playernum++
             if (players[i] == 0) {
-                // if (players[0] == 0 && players[1] == 0 && players[2] == 0 && players[3] == 0){ // assigns a host to the first client available
-                //     socket.emit("new_host");
-                // }
                 players[i] = id; // stores each clients id in players array
                 break;
             }
@@ -65,17 +164,7 @@ io.on("connection", (socket) => { // creates socket.io connection
     socket.emit("receive_index", playernum) // sending player number to client, determines move direction
     console.log(players)
     
-    // socket.on("send_move", (data) => { // receives position from client 
-    //     currentv = data.v;
-    //     currenth = data.h;
-    //     socket.broadcast.emit("receive_move", data) // sends new position to other clients
-    // });
-    
-    // socket.on("send_input", (data) => {  // sends key pressed to other clients
-    //     socket.broadcast.emit("receive_input", data)
-    // });
-    
-    socket.on("send_input", (data) => { //recieves key press from server
+    socket.on("send_input", (data) => { //recieves key press from client
         d[lastinput] = false;
         d[data] = true;
         lastinput = data;
@@ -86,31 +175,38 @@ io.on("connection", (socket) => { // creates socket.io connection
         for (var i = 0; i < 4; i++) {
             if (players[i] == id) {
                 players[i] = 0;
-                // for (var i in players){
-                    //     if (i !== 0){
-                        //         io.to (players[i]).emit('new_host') // assigns 
-                        //     }
-                        // }
                 break;
             }
         }
         console.log(players)
     })
     
-    setInterval(function () { // updates and sends new position to server at a set interval
-    
-        var h = newh(currenth, 37, 39);
-    
-        var v = newv(currentv, 38, 40);
-    
-        if (currentv !== v || currenth !== h) {
-            socket.broadcast.emit("receive_move", { v: v, h: h })
-            currentv = v;
-            currenth = h;
+    setInterval(function () { // updates and sends new position to clients at a set interval
+        socket.emit("item_state", {i1:itemStates[0], i2:itemStates[1], i3:itemStates[2], i4:itemStates[3], i5:itemStates[4]});
+        socket.emit("current_score", {s: score});
+        moveV = newv(currentv, 38, 40);
+        moveH = newh(currenth, 37, 39);
+        
+        if(hasReset){
+            d[lastinput] = false;
+            socket.broadcast.emit("receive_move", { v: currentv, h: currenth })
+            hasReset = false;
         }
-    }, 50); // interval 20msawwadawd
+        else if (currentv !== moveV || currenth !== moveH) {
+            socket.broadcast.emit("receive_move", { v: moveV, h: moveH })
+            currentv = moveV;
+            currenth = moveH;
+        }
+        
+        var i = itemCheck(moveV, moveH);
+        if(i != 0){
+            score++;
+            console.log("score: " + score);
+            socket.broadcast.emit("collect_item", i);
+        }
+    }, 50); // interval 50ms
 })
 
-server.listen(3000, "0.0.0.0", () => { // server listens for connections on port 8080
+server.listen(8080, "0.0.0.0", () => { // server listens for connections on port 8080
     console.log("Server running ... ")
 })
